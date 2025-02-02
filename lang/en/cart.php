3d0ee0d5827da620f46b7700c77ee38c3813ebee
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-02-02 17:37:41 +0100

    style the cart

---
+++ lang/en/cart.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cart Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'cart' => 'Cart',
+];
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,11 +1,84 @@
 <x-app-layout>
 
-    {{-- {{ dd($cartItems) }} --}}
+    @vite(['resources/css/cart.css'])
 
-    @foreach ($cartItems as $cartItem)
-        <div class="product-item">
-            {{ $cartItem["product_id"] }} -- {{ $cartItem["quantity"] }}
+    <div class="cart-wrapper">
+        <h1 class="cart-title">{{ __('cart.cart') }}</h1>
+
+        @foreach ($cartItems as $product)
+            {{-- Loop over cart items to find the product --}}
+            @foreach ($products as $product)
+                @if ($product->id == $product->id)
+                        <div class="cart-item">
+
+                            <img src="{{ $product->image }}" alt="{{ $product->title }}" class="cart-item-image" />
+
+                            <div class="cart-item-details">
+                                <h1>{{ $product->title }}</h1>
+                                {{ $product->price }}
+                            </div>
+
+                            <div class="cart-item-quantity">
+                                <div class="input-wrapper">
+                                    <input type="number" min="1" name="quantity" value="{{ $cartItems[$product->id]['quantity'] }}" id="product-{{ $product->id }}"/>
+                                    <label for="quantity">{{ __('cart.quantity') }}</label>
+                                </div>
+
+                                <button
+                                    type="button"
+                                    onclick='changeQuantity(
+                                        "{{ $product->slug }}",
+                                        "{{ csrf_token() }}",
+                                        "{{ route('cart.update-quantity', $product) }}",
+                                        "{{ "product-" . $product->id }}")'>
+                                    {{ __('cart.update') }}
+                                </button>
+                            </div>
+
+                            <button type="button" class='product-remove-from-cart' onclick='removeProductFromCart("{{ $product->slug }}", "{{ csrf_token() }}", "{{ route('cart.remove', $product->slug) }}")'>
+                                {{ __('cart.remove') }}
+                            </button>
+                    </div>
+                @endif
+            @endforeach
+        @endforeach
+
+        <script>
+            function removeProductFromCart(slug, csrfToken, url) {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', url);
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken);
+                xhr.onload = function (e) {
+                    alert(e.target.responseText);
+                    location.reload();
+                };
+                xhr.send();
+            }
+
+            function changeQuantity(slug, csrfToken, url, inputId) {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', url);
+                const quantity = document.getElementById(inputId).value;
+
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken);
+                xhr.onload = function (e) {
+                    alert(e.target.responseText);
+                    location.reload();
+                };
+                xhr.send(
+                    JSON.stringify({
+                        quantity: quantity
+                    })
+                );
+            }
+
+        </script>
+
+        <div class="cart-total">
+            <h2>{{ __('cart.total') }}</h2>
+            {{ $total }}
         </div>
-    @endforeach
-
+    </div>
 </x-app-layout>
