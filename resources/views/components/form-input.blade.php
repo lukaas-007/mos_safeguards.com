c696685899235d05df4cd5acdbf7dd9db15bd372
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-02-05 00:30:21 +0100

    =update the profile settings

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -5,19 +5,6 @@
     'value' => ''
 ])
 
-@if ($type === 'select')
-    <div class="input-wrapper">
-        <select" name="{{ $name }}" placeholder=" ">
-            @foreach ($options as $option)
-                <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
-            @endforeach
-        </select>
-        <label for="{{ $name }}" class="form-label">{{ $label }}</label>
-    </div>
-
-@endif
-
-
 <div class="input-wrapper">
     <input type="{{ $type }}" name="{{ $name }}" placeholder=" " value='{{ $value }}'>
     <label for="{{ $name }}" class="form-label">{{ $label }}</label>
