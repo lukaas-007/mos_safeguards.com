a2836c50c3ea2151c7d69ab0d8760fb51b672aed
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-02-24 22:26:31 +0100

    add serntry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\DetectBrowserLanguage;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(DetectBrowserLanguage::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        Integration::handles($exceptions);
     })->create();
