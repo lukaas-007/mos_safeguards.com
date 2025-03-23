557b9b626516ed9e2046b6432ff598cd9c78bea0
Lucas van Briemen <vanbriemenlucas@gmail.com>
2025-03-23 16:08:53 +0100

    Add an footer

--- config/app.php
+++ config/app.php
@@ -123,4 +123,5 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'author' => env('APP_AUTHOR', 'Author Name')
 ];
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,5 @@
 <footer>
-    footer
+    {{-- App name --}}
+    <p> {{ config('app.name') }} © {{ date('Y') }} </p>
+    <p> {{ __('footer.made_by') }} {{ config('app.author') }} </p>
 </footer>
